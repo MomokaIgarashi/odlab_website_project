7f4fba4e7de96dadef2db6dfbdd31955cde07a28
Megumikawa <megumi19901016@gmail.com>
2021-10-10 11:41:29 +0200

    add each class name

--- home.php
+++ home.php
@@ -7,67 +7,67 @@
                 <source media="(max-width: 600px)" srcset="<?php echo get_template_directory_uri(); ?>/assets/images/members-page/top-sp.png">
                 <img src="<?php echo get_template_directory_uri(); ?>/assets/images/members-page/top-pc.png" class="members-page-backgroundimage" alt="ceo-image">
             </picture>
-            <div class="members-top-description">
-                <h1 class="members-page__top-description-title">OD Lab合同会社代表</h1>
-                <h2 class="members-page__top-description-title-additional">Organization Development　Network Japan理事<br>株式会社　スコラ・コンサルト　パートナー</h2>
-                <p class="members-page__top-description-japanese-name">水迫　洋子</p>
-                <p class="members-page__top-description-english-name">MIZUSAKO YOKO</p>
+            <div class="members-top-outline">
+                <h1 class="members-page__top-outline--title">OD Lab合同会社代表</h1>
+                <h2 class="members-page__top-outline--title-additional">Organization Development　Network Japan理事<br>株式会社　スコラ・コンサルト　パートナー</h2>
+                <p class="members-page__top-outline__jp-name">水迫　洋子</p>
+                <p class="members-page__top-outline__en-name">MIZUSAKO YOKO</p>
             </div>
         </div>
         <div class="members-main">
-            <section class="members-page__main-description">
-                <ul>
-                    <li>
-                        <h3　class="members-page__main-description--title">経歴</h3>
-                        <p>青山学院大学院 社会情報学部　ヒューマンイノベーション学科修士課程修了。野村證券を経て、東京電力の新規事業としての子会社３社の事業会社の立ち上げ、事業拡大の経験の中、企業変革の重要性を感じスコラ・コンサルトへ。2008年より2015年までスコラ・コンサルトの代表を務めたのち、独立。ODNJでは「理事」および「研究会委員長」を務め、「組織開発基礎講座」も担当。</p>
+            <section class="members-page__main-outline">
+                <ul class="members-page__main-section">
+                    <li class="members-page__main-section--list">
+                        <h3　class="members-page__main-section__title">経歴</h3>
+                        <p class="members-page__main-section__career-detail">青山学院大学院 社会情報学部　ヒューマンイノベーション学科修士課程修了。野村證券を経て、東京電力の新規事業としての子会社３社の事業会社の立ち上げ、事業拡大の経験の中、企業変革の重要性を感じスコラ・コンサルトへ。2008年より2015年までスコラ・コンサルトの代表を務めたのち、独立。ODNJでは「理事」および「研究会委員長」を務め、「組織開発基礎講座」も担当。</p>
                     </li>
-                    <li>
-                        <h3 class="members-page__main-description--title">強み</h3>
-                        <ul>
-                            <li>日本を代表する大企業の組織開発や組織変革の多数の実績</li>
-                            <li>役員チームや事業リーダー層など、社内の人間だけでは関わることが難しいトップ層への関わりの実績多数</li>
-                            <li>企業内における組織開発コーディネーターの養成</li>
+                    <li class="members-page__main-section--list">
+                        <h3 class="members-page__main-section__title">強み</h3>
+                        <ul class="members-page__main-section__strength">
+                            <li class="members-page__main-section__strenght-list">日本を代表する大企業の組織開発や組織変革の多数の実績</li>
+                            <li class="members-page__main-section__strength-list">役員チームや事業リーダー層など、社内の人間だけでは関わることが難しいトップ層への関わりの実績多数</li>
+                            <li class="members-page__main-section__strength-list">企業内における組織開発コーディネーターの養成</li>
                         </ul>
                     </li>
-                    <li>
-                        <h3 class="members-page__main-description--title">分野</h3>
-                        <ul>
-                            <li>経営チームづくり</li>
-                            <li>トップ・ミドル層のマネジメント変革</li>
-                            <li>組織開発事務局支援</li>
-                            <li>組織開発コーディネーターの養成</li>
-                            <li>ダイバーシティ推進</li>
-                            <li>ビジョン浸透</li>
-                            <li>自律型組織</li>
-                            <li>変革リーダー育成</li>
-                            <li>組織風土改革</li>
+                    <li class="members-page__main-section--list">
+                        <h3 class="members-page__main-section__title">分野</h3>
+                        <ul class="members-page__main-section__field">
+                            <li class="members-page__main-section__field-list">経営チームづくり</li>
+                            <li class="members-page__main-section__field-list">トップ・ミドル層のマネジメント変革</li>
+                            <li class="members-page__main-section__field-list">組織開発事務局支援</li>
+                            <li class="members-page__main-section__field-list">組織開発コーディネーターの養成</li>
+                            <li class="members-page__main-section__field-list">ダイバーシティ推進</li>
+                            <li class="members-page__main-section__field-list">ビジョン浸透</li>
+                            <li class="members-page__main-section__field-list">自律型組織</li>
+                            <li class="members-page__main-section__field-list">変革リーダー育成</li>
+                            <li class="members-page__main-section__field-list">組織風土改革</li>
                         </ul>
                     </li>
-                    <li>
-                        <h3 class="members-page__main-description--title">実績</h3>
-                        <div>
-                            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/members-page/works1.png" class="" alt="works1">
+                    <li class="members-page__main-section--list">
+                        <h3 class="members-page__main-section__title">実績</h3>
+                        <div class="members-page__main-section__works">
+                            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/members-page/works1.png" class="members-page__works-image" alt="works1">
                             <h4>次世代経営者育成・経営チームづくり</h4>
                             <p>IT企業、物流、電力、アパレルメーカー、半導体測定機メーカー、情報サービス業、通信ネットワーク業、機械メーカー、</p>
                         </div>
-                        <div>
-                            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/members-page/works2.png" class="" alt="works2">
+                        <div class="members-page__main-section__works">
+                            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/members-page/works2.png" class="members-page__works-image" alt="works2">
                             <h4>組織開発支援</h4>
                             <p>総合商社、総合化学メーカー、飲食業、損害保険業、IT企業、総合人材サービス業、生活協同組合、電機メーカー</p>
                         </div>
-                        <div>
-                            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/members-page/works3.png" class="" alt="works3">
+                        <div class="members-page__main-section__works">
+                            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/members-page/works3.png" class="members-page__works-image" alt="works3">
                             <h4>マネジャー育成、変革リーダー育成</h4>
                             <p>自動車メーカー、精密機器メーカー、医薬品メーカー、総合商社、飲料メーカー、損害保険業、不動産、総合商社、銀行、電機メーカー</p>
                         </div>
-                        <div>
-                            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/members-page/works4.png" class="" alt="works4">
+                        <div class="members-page__main-section__works">
+                            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/members-page/works4.png" class="members-page__works-image" alt="works4">
                             <h4>ダイバーシティ推進・ビジョン浸透・自律型組織</h4>
                             <p>IT企業、飲食業、物流、生活協同組合、総合人材サービス業</p>
                         </div>
                     </li>
                     <li>
-                        <h3　class="members-page__main-description--title">著書</h3>
+                        <h3　class="members-page__main-description__title">著書</h3>
                         <img src="<?php echo get_template_directory_uri(); ?>/assets/images/members-page/works4.png" class="" alt="works4">
                         <p>クロネコヤマト人の育て方</p>
                         <p>著者　水迫洋子</p>
@@ -83,20 +83,20 @@
             </section>
         </div>
         <div class="members-partner">
-            <ul>
-                <li>
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/members-page/partner1.png" class="members-page__main-image" alt="SCHOLAR CONSULT">
-                    <h5>パートナー</h5>
-                    <p>スコラ・コンサルタント</p>
-                    <p>SCHOLAR CONSULT</p>
-                    <p>組織風土の変革、新しい組織文化の創造をサポートするスコラ・コンサルト組織風土改革のパイオニアとして、大企業から中小企業までの組織の変革をご支援いたします。</p>
+            <ul class="members-partner__outline">
+                <li class="members-partner__outline-list">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/members-page/partner1.png" class="members-page__partner-image" alt="SCHOLAR CONSULT">
+                    <h5 class="members-partner__title">パートナー</h5>
+                    <p class="members-partner__js-name">スコラ・コンサルタント</p>
+                    <p class="members-partner__en-name">SCHOLAR CONSULT</p>
+                    <p class="members-partner__detail">組織風土の変革、新しい組織文化の創造をサポートするスコラ・コンサルト組織風土改革のパイオニアとして、大企業から中小企業までの組織の変革をご支援いたします。</p>
                 </li>
-                <li>
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/members-page/partner2.png" class="members-page__main-image" alt="FUJITA HARUNO">
-                    <h5>パートナー / テクスト株式会社 代表取締役</h5>
-                    <p>藤田 ハルノ</p>
-                    <p>FUJITA HARUNO</p>
-                    <p>国際基督教大学 教養学部 国際関係学科を卒業後、知的資本経営を掲げる株式会社 ICMG にコンサルタント職として入社。
+                <li class="members-partner__outline-list">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/members-page/partner2.png" class="members-page__partner-image" alt="FUJITA HARUNO">
+                    <h5 class="members-partner__title">パートナー / テクスト株式会社 代表取締役</h5>
+                    <p class="members-partner__js-name">藤田 ハルノ</p>
+                    <p class="members-partner__en-name">FUJITA HARUNO</p>
+                    <p class="members-partner__detail">国際基督教大学 教養学部 国際関係学科を卒業後、知的資本経営を掲げる株式会社 ICMG にコンサルタント職として入社。
                         一般社団法人 Japan Innovation Network の立ち上げ、企業の新規事業創造エコシステムのデザインプロジェクトに関わったのち、
                         ミラノにある大学院ドムスアカデミーのビジネスデザイン修士へ留学。
                         その後、言葉のにわづくりをコンセプトとして、テクスト株式会社を創業し、言葉を可視化することでコミュニケーションを支援するグラフィックスキルや、
