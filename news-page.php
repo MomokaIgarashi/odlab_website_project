612d1917c5fed70455dff421a0b69b6bda9a5167
MomokaIgarashi <migarashi0318@gmail.com>
2022-04-22 14:50:00 +0200

    Created html sections

--- news-page.php
+++ news-page.php
@@ -5,5 +5,14 @@ Template Name: News Page
 ?>
 <?php get_header(); ?>
 
-
+<div class="news-index">
+    <div class="news-index-title">
+        <h1　class="top-index__title">お知らせ</h1>
+    </div>
+    <div class="news-index-content">
+        <div class="news-index-content-category"></div>
+        <div class="news-index-content-articles"></div>
+        <div class="news-index-content-pagenation"></div>
+    </div>
+</div>
 <?php get_footer(); ?>
