e5d3d238f0fc92d36dd15bebb8962c98b570fe29
MomokaIgarashi <migarashi0318@gmail.com>
2022-04-23 18:23:12 +0200

    single page PC frontend

--- single.php
+++ single.php
@@ -1,24 +1,38 @@
 <?php get_header();?>
 
 <div class="post--wrapper">
-	<img src="<?php echo get_template_directory_uri(); ?>/assets/images/front-page/NEWS.png" class="post--image__title" alt="">
-	<?php if ( has_post_thumbnail() ) { ?>
-		<?php the_post_thumbnail('post-thumbnail', array('class' => 'post--photo') ); ?>
-		<?php }else{ ?>
-		<img class="post--photo" src="<?php echo get_template_directory_uri(); ?>/assets/images/post/thumbnail-news.JPG">
-	<?php }?>
-	<div class="post--article__title">
-		<?php the_title(); ?>
-	</div>
-	<div class="post--article__date">
-		<?php the_time('Y/m/d'); ?>
-	</div>
-	<div class="post--article__content">
-		<?php the_content( $more_link_text, $stripteaser ); ?>
-	</div>
-	<a class="post--article__button" href="<?= esc_url(home_url('/'));?>">
-		トップページに戻る
-	</a>
+
+    <div class="post--back__inner">
+        <img class="post--back__icon" src="<?php echo get_template_directory_uri(); ?>/assets/images/news/back.png">
+        <a href="<?= esc_url(home_url('/news'));?>" class="post--back__text">一覧ページへ</a>
+    </div>
+
+    <div class="post-article--title__inner">
+        <button class="index--news--article--text__category">
+            <?php the_category(array('class' => 'index--news--article--text__category') ); ?>
+        </button>
+
+        <div class="post--article__title">
+                <?php the_title(); ?>
+        </div>
+        <div class="post--article__date">
+                <?php the_time('Y/m/d'); ?>
+        </div>
+    </div>
+
+    <div class="post--article--content__inner">
+        <?php if ( has_post_thumbnail() ) { ?>
+            <?php the_post_thumbnail('post-thumbnail', array('class' => 'post--photo') ); ?>
+            <?php }else{ ?>
+                <img class="index--news--photo" src="<?php echo get_template_directory_uri(); ?>/assets/images/news/no-thumbnais.png">
+            <?php }?>
+        <div class="post--article--content__wrapper">
+            <div class="post--article__content">
+                <?php the_content( $more_link_text, $stripteaser ); ?>
+            </div>
+        </div>
+    </div>
+
 </div>
 
 <?php get_footer();
